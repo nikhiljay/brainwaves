07f3a7ffe7323da06970dcfa1eb97225cae988f7
Nikhil D'Souza <nikhiljay7@gmail.com>
2016-04-01 11:14:58 -0700

    Added iOS App Project. Next step is to configure Firebase with python and iOS.

---
+++ Brainwaves/Brainwaves/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Brainwaves
+//
+//  Created by Nikhil D'Souza on 4/1/16.
+//  Copyright © 2016 Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var outputText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
