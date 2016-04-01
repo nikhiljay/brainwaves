6d9fe9914a797d28cb9c44ae376612c425a36e3a
Nikhil D'Souza <nikhiljay7@gmail.com>
2016-04-01 15:32:19 -0700

    Added iOS Charts Cocoapod.

--- Brainwaves/Brainwaves/HomeViewController.swift
+++ Brainwaves/Brainwaves/HomeViewController.swift
@@ -14,8 +14,35 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        outputText.allowsEditingTextAttributes = false
+        let myUrl = NSURL(string: "https://da6ebb8b.ngrok.io");
+        let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "GET"
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+//            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+//            print(responseString!)
+            
+            do {
+                if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                    print(convertedJsonIntoDict)
+                    
+                    let value = convertedJsonIntoDict["Y"]!["quality"] as? Int
+                    print(value!)
+                    
+                    dispatch_async(dispatch_get_main_queue(), {() -> Void in
+                        self.outputText.text = String(value!)
+                    })
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
     }
     
 }
---
+++ Brainwaves/Pods/Target Support Files/Pods-Brainwaves/Pods-Brainwaves-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Brainwaves : NSObject
+@end
+@implementation PodsDummy_Pods_Brainwaves
+@end
